e7ed997baac2ba5cc0976cecf90d1e3184174f5c
diogo-aparecido-smartfit <diogo.amv19@gmail.com>
2025-05-14 14:20:09 -0300

    feat: update baseurl

--- app/services/api/http.client.ts
+++ app/services/api/http.client.ts
@@ -1,26 +1,29 @@
-import axios from "axios";
+import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: "https://agilmed-api.vercel.app/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+    baseURL: 'agilmed-api.azurewebsites.net/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
 
 httpClient.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    console.error("Erro na requisição:", error.response?.data || error.message);
-    return Promise.reject(error);
-  }
-);
+    (response) => response,
+    async (error) => {
+        console.error(
+            'Erro na requisição:',
+            error.response?.data || error.message
+        )
+        return Promise.reject(error)
+    }
+)
 
 httpClient.interceptors.request.use((config) => {
-  const token = "";
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+    const token = ''
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
 
-export default httpClient;
+export default httpClient
